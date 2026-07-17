fa3ed28e4af0e46e80007c07637fcaca7b398250
Wgy <904625946@qq.com>
2026-07-17 19:24:48 +0800

    fix: initialize agent tools package

--- app/agent/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.agent.tools.completeness import check_completeness
-
-
-__all__ = [
-    "check_completeness",
-]
---
+++ app/agent/tools/__init__.py
@@ -0,0 +1,8 @@
+from app.agent.tools.ambiguity import check_ambiguity
+from app.agent.tools.completeness import check_completeness
+
+
+__all__ = [
+    "check_ambiguity",
+    "check_completeness",
+]
