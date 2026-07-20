369625f174c3b76f47d85f678295bc63a05aa866
Wgy <904625946@qq.com>
2026-07-20 15:51:14 +0800

    refactor: manage database schema with Alembic

--- app/database.py
+++ app/database.py
@@ -1,10 +1,14 @@
+import os
 from collections.abc import Generator
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import DeclarativeBase, Session, sessionmaker
 
 
-DATABASE_URL = "sqlite:///./reqflow.db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "sqlite:///./reqflow.db",
+)
 
 
 engine = create_engine(
--- app/main.py
+++ app/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 
-from app.database import Base, engine
 from app.routers.requirements import router as requirements_router
 
-
-Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI(
